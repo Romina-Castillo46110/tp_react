a1737c1b5b632c151c7f036ad83b0b11aac2b30e
Romina-Castillo46110 <romina.castillo195@gmail.com>
2023-11-07 23:47:36 -0300

    version3

--- src/pages/Componentes.tsx
+++ src/pages/Componentes.tsx
@@ -1,9 +1,11 @@
 import DangerInput from "../components/DangerInput/DangerInput.tsx";
+import AlertMessage from "../components/AlertMessage/AlertMessage.tsx";
 
 const Componentes = () => {
     return (
         <div>
             <DangerInput/>
+            <AlertMessage/>
         </div>
     )
 }
