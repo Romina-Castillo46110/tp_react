6046f288a8ce47d6e6927e16ea7c44d19a99fd5c
Romina-Castillo46110 <romina.castillo195@gmail.com>
2023-11-08 16:07:07 -0300

    version4

--- src/pages/Componentes.tsx
+++ src/pages/Componentes.tsx
@@ -1,11 +1,13 @@
 import DangerInput from "../components/DangerInput/DangerInput.tsx";
 import AlertMessage from "../components/AlertMessage/AlertMessage.tsx";
+import ButtonColorPick from "../components/ButtonColorPick/ButtonColorPick.tsx";
 
 const Componentes = () => {
     return (
         <div>
             <DangerInput/>
             <AlertMessage/>
+            <ButtonColorPick/>
         </div>
     )
 }
