b3ec5ed7d1d971d9152fdaabe91d8979805498a2
Romina-Castillo46110 <romina.castillo195@gmail.com>
2023-11-10 01:03:19 -0300

    version5

--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -2,7 +2,10 @@ import {useEffect, useState} from "react";
 import {Product} from "../../types/Product.ts";
 import {ProductService} from "../../services/ProductService.ts";
 import Loader from "../Loader/Loader.tsx";
-import {Table} from "react-bootstrap";
+import {Button, Table} from "react-bootstrap";
+import {ModalType} from "../../types/ModalType.ts";
+import ProductModal from "../ProductModal/ProductModal.tsx";
+
 
 const ProductTable = () => {
     const[products, setProducts] = useState<Product[]>([]);
@@ -16,8 +19,29 @@ const ProductTable = () => {
         fetchProducts();
     }, []);
     console.log(JSON.stringify(products, null, 2));
+    const initializableNewProduct = (): Product => {
+        return {
+            id: 0,
+            title: "",
+            price: 0,
+            description: "",
+            category: "",
+            image: "",
+        };
+    };
+            const [product, setProduct] = useState<Product>(initializableNewProduct);
+            const [showModal, setshowModal] = useState(false);
+            const [modalType, setModalType] = useState<ModalType>(ModalType.NONE);
+            const [title, setTitle] = useState("");
+            const handleClick = (newTitle: string, prod: Product, modal: ModalType) => {
+                setTitle(newTitle);
+                setModalType(modal);
+                setProduct(prod);
+                setshowModal(true);
+            };
     return (
         <>
+            <Button onClick={() => handleClick("Nuevo producto", initializableNewProduct(), ModalType.CREATE)}> Nuevo Producto </Button>
             {isLoading ? <Loader/> : (
                 <Table hover>
                     <thead>
@@ -36,13 +60,34 @@ const ProductTable = () => {
                                 <td>{product.price}</td>
                                 <td>{product.description}</td>
                                 <td>{product.category}</td>
-                                <td>img src={product.image} alt={product.title} style={{width: '50px'}}</td>
-                        </tr>
+                                 <td><img src={product.image} alt={product.title} style={{ width: '50px' }} /></td>
+                             </tr>
                         ))}
                     </tbody>
                 </Table>
+            )}
+            {showModal && (
+                <ProductModal
+                    show={showModal}
+                    onHide={() => setshowModal(false)}
+                    title={title}
+                    modalType={modalType}
+                    prod={product}
+                    />
             )}
         </>
     )
 }
 export default ProductTable
+
+
+
+
+
+
+
+
+
+
+
+
